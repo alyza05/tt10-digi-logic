94a66eb4aa44d4f68d12b6e9a819d26d53f5664b
alyza05 <alyzaalfonso@gmail.com>
2025-02-27 18:41:02 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -16,10 +16,34 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  wire [15:0] In = {ui_in, uio_in};
+  reg [7:0] C;
+
+  always_comb begin
+      casez (In)
+          16'b1???????????????: C = 8'd15;
+          16'b01??????????????: C = 8'd14;
+          16'b001?????????????: C = 8'd13;
+          16'b0001????????????: C = 8'd12;
+          16'b00001???????????: C = 8'd11;
+          16'b000001??????????: C = 8'd10;
+          16'b0000001?????????: C = 8'd9;
+          16'b00000001????????: C = 8'd8;
+          16'b000000001???????: C = 8'd7;
+          16'b0000000001??????: C = 8'd6;
+          16'b00000000001?????: C = 8'd5;
+          16'b000000000001????: C = 8'd4;
+          16'b0000000000001???: C = 8'd3;
+          16'b00000000000001??: C = 8'd2;
+          16'b000000000000001?: C = 8'd1;
+          16'b0000000000000001: C = 8'd0;
+          default:               C = 8'b1111_0000; // Special case when no 1 is found
+      endcase
+  end
+
+  assign uo_out = C;
   assign uio_out = 0;
-  assign uio_oe  = 0;
+  assign uio_oe = 0;
 
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
